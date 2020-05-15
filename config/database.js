92cb780665fb74120255ccce546f0c225e92063f
an-apluss <boscotec.inc@gmail.com>
2020-05-15 19:46:39 +0100

    setup database

--- config/database.js
+++ config/database.js
@@ -19,15 +19,15 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  // connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'sqlite'),
 
-  connection: {
-    host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-  }, 
+  // connection: {
+  //   host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+  //   port: Env.get('DB_PORT', ''),
+  //   user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+  //   password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+  //   database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+  // }, 
 
   /*
   |--------------------------------------------------------------------------
@@ -61,11 +61,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
     }
   },
 
